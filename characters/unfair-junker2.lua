497d3f47bdfccaf2e5c1e76ea1ae32ad158b903f
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-15 06:30:07 -0400

    Update unfair-junker2.lua

--- characters/unfair-junker2.lua
+++ characters/unfair-junker2.lua
@@ -1,7 +1,7 @@
 local healthtolower = 0.02;
 
 function opponentNoteHit(membersIndex, noteData, noteType, isSustainNote)
-    if dadName == 'unfair-junker' then
+    if dadName == 'unfair-junker2' then
         setProperty('health', getProperty('health') -(healthtolower / 12))
     end
 end
